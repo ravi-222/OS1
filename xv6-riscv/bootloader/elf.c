a207947ba9d7fa002b77796974e51905b0f579a1
Ravindersingh Anand <50708908+ravi-222@users.noreply.github.com>
2023-09-09 23:34:21 -0700

    Update elf.c

--- xv6-riscv/bootloader/elf.c
+++ xv6-riscv/bootloader/elf.c
@@ -8,20 +8,64 @@
 
 #include <stdbool.h>
 
-struct elfhdr* kernel_elfhdr;
-struct proghdr* kernel_phdr;
+// struct elfhdr *kernel_elfhdr;
+// struct proghdr *kernel_phdr;
+uint64 addr;
 
-uint64 find_kernel_load_addr(enum kernel ktype) {
+uint64 find_kernel_load_addr(enum kernel ktype)
+{
     /* CSE 536: Get kernel load address from headers */
-    return 0;
+
+    if (ktype == NORMAL)
+    {
+        addr = RAMDISK;
+    }
+    else if (ktype == RECOVERY)
+    {
+        addr = RECOVERYDISK;
+    }
+    struct elfhdr *kernel_elfhdr = (struct elfhdr *)addr;
+    uint64 phoff = kernel_elfhdr->phoff;
+    uint64 phentsize = kernel_elfhdr->phentsize;
+    struct proghdr *text_proghdr = (struct proghdr *)(addr + phoff + phentsize);
+    uint64 kernload_start = text_proghdr->vaddr;
+
+    return kernload_start;
 }
 
-uint64 find_kernel_size(enum kernel ktype) {
+uint64 find_kernel_size(enum kernel ktype)
+{
     /* CSE 536: Get kernel binary size from headers */
-    return 0;
+
+    if (ktype == NORMAL)
+    {
+        addr = RAMDISK;
+    }
+    else if (ktype == RECOVERY)
+    {
+        addr = RECOVERYDISK;
+    }
+    struct elfhdr *kernel_elfhdr = (struct elfhdr *)addr;
+    uint64 shentsize = kernel_elfhdr->shentsize;
+    uint64 shnum = kernel_elfhdr->shnum;
+    uint64 kernel_size = kernel_elfhdr->shoff + shentsize * shnum;
+
+    return kernel_size;
 }
 
-uint64 find_kernel_entry_addr(enum kernel ktype) {
+uint64 find_kernel_entry_addr(enum kernel ktype)
+{
     /* CSE 536: Get kernel entry point from headers */
-    return 0;
+
+    if (ktype == NORMAL)
+    {
+        addr = RAMDISK;
+    }
+    else if (ktype == RECOVERY)
+    {
+        addr = RECOVERYDISK;
+    }
+    struct elfhdr *kernel_elfhdr = (struct elfhdr *)addr;
+    uint64 kernel_entry = kernel_elfhdr->entry;
+    return kernel_entry;
 }
